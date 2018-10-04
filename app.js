b9373b2a104526b5c30ec7356967247eb5ee25d1
Savio <savio_furtado@hotmail.com>
2018-10-04 06:53:19 +0000

    before route refactoring

--- app.js
+++ app.js
@@ -5,43 +5,49 @@
 // npm install body-parser --save
 // npm install mongoose --save
 
-var express = require("express");
-var app = express();
-var Campground= require("./models/campground");
-var seedDB = require("./seeds");
-
-// body parser include
+var express         = require("express");
+var app             = express();
+var Campground      = require("./models/campground");
+var seedDB          = require("./seeds");
+var Comment         = require("./models/comment");
+var passport        = require("passport");
+var LocalStrategy   = require("passport-local");
+var User            = require("./models/user")
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
+app.use(express.static(__dirname +"/public"));
 
 //connecting to DB
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
 
-
 seedDB();
- /*Campground.create({
-    name: "Granite Hill", 
-    image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f3c070a4e5b4b9_340.jpg",
-    description: "This is a beautiful Granite Mountain"
-    },function(err, campground){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("New Campground Created");
-            console.log(campground);
-            
-       }
-    }); */
 
-/* var campgrounds = [
-        {name: "Salmon Creek", image:"https://farm2.staticflickr.com/1166/1359158410_46d778223f.jpg"},
-        {name: "Granite Hill", image:"https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f2c87aa0eeb4be_340.jpg"},
-        {name: "Mountain Goat's Rest", image:"https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f2c87aa0eeb4be_340.jpg"}
-        ]*/
+
+//Passport Configuration
+    app.use(require("express-session")({
+        secret: "Just Testing",
+        resave: false,
+        saveUninitialized: false
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(new LocalStrategy(User.authenticate()));
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
+
+    // middleware to pass currentUser to all routes
+    app.use(function(req, res, next){
+        res.locals.currentUser = req.user;
+        next();
+    });
+
+
+//================
+//  Routes
+//================
 
 app.get("/", function(req, res){
       res.render("landing");
@@ -53,7 +59,7 @@ app.get("/campgrounds", function(req,res){
             if(err){
                 console.log(err);
             } else {
-                res.render("index", {campgrounds: allCampgrounds});
+                res.render("campgrounds/index", {campgrounds: allCampgrounds, currentUser: req.user});
             }
         });
 });
@@ -76,7 +82,7 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", function(req, res){
@@ -86,12 +92,114 @@ app.get("/campgrounds/:id", function(req, res){
         if(err){
             console.log("error");
         } else {
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
         
 });
+
+//===================
+// Comment Routes
+//===================
+
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     
+    // find campground
+    Campground.findById(req.params.id, function(err, campground){
+        if (err){
+            console.log("error finding campground");}
+        else {
+                res.render("comments/new", {campground: campground});
+        }
+        
+    });
+   
+});
+    
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
+    
+    // find campground using id
+    
+    Campground.findById(req.params.id, function(err, campground){
+        if (err){
+            console.log("error finding campground");
+            res.redirect("/campgrounds");
+        }
+        else {
+                // create comment
+                Comment.create(req.body.comment, function(err, comment){
+                     if (err){
+                        console.log("error comment");
+                         res.redirect("/campgrounds");
+                     } else {
+                         // connect comment t campground
+                         campground.comments.push(comment);
+                         campground.save();
+                         
+                         // redirect to show pageres.render("comments/new", {campground: campground});
+                         res.redirect("/campgrounds/"+ campground._id);
+                
+                     }
+                     
+                });
+                
+                
+        
+             
+         }
+    });
+   
+});
+
+//================
+// Auth Routes
+//================
+
+// Show Register Form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+ // req.body.username;
+ // req.body.password;
+ var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+      if(err){
+          console.log("error");
+          res.render("register");
+      }
+      passport.authenticate("local")(req, res, function(){
+          res.redirect("/campgrounds");
+      });
+  });
+});
+
+app.get("/login", function( req, res){
+    res.render("login");
+});
+
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req, res){
+    
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+
+function isLoggedIn(req,res, next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
+ 
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
--- seeds.js
+++ seeds.js
@@ -5,15 +5,21 @@ var Comment= require("./models/comment");
 var data = [
         {name: "Salmon Creek",
             image:"https://farm2.staticflickr.com/1166/1359158410_46d778223f.jpg",
-            description:"Campground1"},
-        {name: "Granite Hill",
-            image:"https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f7c878a6ebb4bf_340.jpg",
-            description:"Campground3"  
+            description:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+        },
+        {name: "Hailey Idaho",
+            image:"https://tse1.mm.bing.net/th?id=OIP.57Of0lLd0WUZYe5L9vhScwHaFj&w=151&h=105&c=8&rs=1&qlt=90&dpr=1.125&pid=3.1&rm=2",
+            description:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." 
         },
         {name: "Mountain Goat's Rest",
             image:"https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg",
-            description:"Campground3"
+            description:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+        },
+        {name: "Milwaukee",
+            image:"https://tse1.mm.bing.net/th?id=OIP.Ww32HNgatE5NLs71-1l7VAHaFj&w=151&h=105&c=8&rs=1&qlt=90&dpr=1.125&pid=3.1&rm=2",
+            description:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
         }
+        
         ]
 
 function seedDB(){
